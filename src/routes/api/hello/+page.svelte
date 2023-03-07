882ee494ea89d3e2ad8ca9639fa0206c83f9ae79
Mark McAvoy <amgs24934@gmail.com>
2023-03-07 22:53:30 +0000

    try to fetch from the backend

---
+++ src/routes/api/hello/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let data;
+    fetch('https://3000-mk24934-multilingualsit-irqgvoc1un5.ws-us89b.gitpod.io')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+</script>
+
+<!-- {data} -->
+
+
