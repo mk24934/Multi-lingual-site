4d77602c896d7cd01a32de7cbc3db1ceb27340fd
Mark McAvoy <amgs24934@gmail.com>
2023-04-18 02:51:57 +0000

    try for post request

---
+++ Notes/Apr-20.md
@@ -0,0 +1,4 @@
+# April 20th
+
+- In Nest it is using something called a decorator, such as: `@Injectable`, but if it is going to be added within the decorator `@Module` with the named key `providers`, is it really needed?
+- How to make a Post request? We had made the hello `+page.server.ts` post file, however how to make a post to the Nest server on Digital Ocean that is waiting for a Post request and how to send data?
--- src/routes/[lang]/hello/+page.server.ts
+++ src/routes/[lang]/hello/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from "./$types"
 
 // fetch within here is a magic version of fetch, a wrapper around, so it is same plus additional things
 export const load: PageServerLoad = async ({ fetch }) => {
-    const message: string = await fetch('http://134.209.248.214:3000') // DigitalOcean link
+    const message: string = await fetch('http://134.209.248.214:3000/day') // DigitalOcean link
         .then(response => response.text())
         // then passes input from previous into next, so if add a console.log in a .then here will change the return type to void
     console.log('message is: ', message)
